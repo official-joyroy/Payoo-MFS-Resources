8b404e724380a6036f6042d29b18987859280421
unknown <joyr86150@gmail.com>
2025-11-01 13:29:55 +0600

    y commit

---
+++ home.js
@@ -0,0 +1,14 @@
+// stap 1 
+document.getElementById('button-admany').addEventListener('click', function (event){
+    event.preventDefault();
+    // stap2
+    const addmonayinput = document.getElementById('input-ad-many').value;
+    console.log(addmonayinput);
+
+    // gat the pin namber
+    const pinnamberinput = document.getElementById('input-pin-namber').value;
+    console.log(pinnamberinput)
+
+    // stap 3
+    
+})
